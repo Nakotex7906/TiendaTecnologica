9084be472f260fbabcce85aa90c5fd7fcbafa01b
BenjaBeroiza <b.beroiza01@ufromail.cl>
2025-04-02 12:25:22 -0300

    Clase cliente con atributos, constructor, getter y setters

--- src/main/java/Modelo/Cliente.java
+++ src/main/java/Modelo/Cliente.java
@@ -3,5 +3,66 @@ package Modelo;
 public class Cliente {
 
     private String nombre;
+    private String apellido;
+    private String correo;
+    private int telefono;
+    private String estadoCivil;
+    private String ciudad;
 
+    public Cliente(String nombre, String apellido, String correo, int telefono, String estadoCivil, String ciudad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.estadoCivil = estadoCivil;
+        this.ciudad = ciudad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
 }

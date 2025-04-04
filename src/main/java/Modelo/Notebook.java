5665a0983a5e9fda640cc7d956c067a60b1dd383
AlonsoRom <w.chavez01@ufromail.cl>
2025-04-04 17:32:06 -0300

    Ajustes en notebook y tablet

--- src/main/java/Modelo/Notebook.java
+++ src/main/java/Modelo/Notebook.java
@@ -6,8 +6,8 @@ public class Notebook extends DispositivoTecnologico {
     private String tipoTeclado;
     private int bateriaMah;
 
-    public Notebook(String marca, String memoriaRAM, String memoriaAlmacenamiento, String procesador, String modelo, int anioP, double precio, int stock, String resolucionPantalla, String tipoTeclado, int bateriaMah) {
-        super(marca, memoriaRAM, memoriaAlmacenamiento, procesador, modelo, anioP, precio, stock);
+    public Notebook(String marca, String memoriaRAM, String memoriaAlmacenamiento, String procesador, String modelo,String tipo, int anioP, double precio, int stock, String resolucionPantalla, String tipoTeclado, int bateriaMah) {
+        super(marca, memoriaRAM, memoriaAlmacenamiento, procesador, modelo, tipo, anioP, precio, stock);
         this.resolucionPantalla = resolucionPantalla;
         this.tipoTeclado = tipoTeclado;
         this.bateriaMah = bateriaMah;
--- src/main/java/Modelo/Tablet.java
+++ src/main/java/Modelo/Tablet.java
@@ -7,8 +7,8 @@ public class Tablet extends DispositivoTecnologico{
     private String resolucionPantalla;
     private List<String> accesorios;
 
-    public Tablet(String marca, String memoriaRAM, String memoriaAlmacenamiento, String procesador, String modelo, int anioP, double precio, int stock, String resolucionPantalla, List<String> accesorios) {
-        super(marca, memoriaRAM, memoriaAlmacenamiento, procesador, modelo, anioP, precio, stock);
+    public Tablet(String marca, String memoriaRAM, String memoriaAlmacenamiento, String procesador, String modelo,String tipo, int anioP, double precio, int stock, String resolucionPantalla, List<String> accesorios) {
+        super(marca, memoriaRAM, memoriaAlmacenamiento, procesador, modelo,tipo, anioP, precio, stock);
         this.resolucionPantalla = resolucionPantalla;
         this.accesorios = accesorios;
     }
